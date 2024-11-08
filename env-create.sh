3b1024c26aff67296de81ad076f5da266a58d89e
kilo <kilo@localhost.localdomain>
2024-11-08 10:14:04 +0100

    Added front related stuff

--- env-create.sh
+++ env-create.sh
@@ -28,7 +28,7 @@ read -p "Enter Product DB host (default: db-product): " PRODUCT_DB_HOST
 PRODUCT_DB_HOST=${PRODUCT_DB_HOST:-db-product}
 
 read -p "Enter Product DB user (default: product_user): " PRODUCT_DB_USER
-PRODUCT_DB_USER=${PRODUCT_DB_USER:-product_user}
+PRODUCT_DB_USER="${PRODUCT_DB_USER:-product_user}"
 
 read -p "Enter Product DB password: " PRODUCT_DB_PASSWORD
 if [ -z "${PRODUCT_DB_PASSWORD}" ]
@@ -46,12 +46,34 @@ then
     JWT_SECRET=$(openssl rand -base64 32)
 fi
 
+
+read -p "What AUTH API URL to use? (default http://localhost:8081) " AUTH_API_URL
+AUTH_API_URL="${AUTH_API_URL:-http://localhost:8081}"
+
+read -p "What PRODUCT API URL to use? (default http://localhost:8082) " PRODUCT_API_URL
+PRODUCT_API_URL="${PRODUCT_API_URL:-http://localhost:8082}"
+
+while true; do
+    read -p "What is the mode of your NODE application? (default development) " NODE_ENV
+    NODE_ENV=${NODE_ENV:-development}
+    if [ "${NODE_ENV}" == "production" ] || [ "${NODE_ENV}" == "development" ]
+        then
+        NODE_ENV=${NODE_ENV}
+        break
+        else
+        echo "Invalid NODE_ENV. Please enter either the word production or development."
+        exit 1
+    fi
+done
+
+
 # Création du fichier .env
 cat << EOF > .env
 # Common
 TIMEZONE=${TIMEZONE}
 GIN_MODE=${GIN_MODE}
-DB_PORT=5432
+DB_AUTH_PORT=5433
+DB_PRODUCT_PORT=5434
 DB_SSLMODE=disable
 
 # Auth Service
@@ -66,4 +88,18 @@ PRODUCT_DB_HOST=${PRODUCT_DB_HOST}
 PRODUCT_DB_USER=${PRODUCT_DB_USER}
 PRODUCT_DB_PASSWORD=${PRODUCT_DB_PASSWORD}
 PRODUCT_DB_NAME=${PRODUCT_DB_NAME}
+
+# Front Service
+AUTH_API_URL=${AUTH_API_URL}
+PRODUCT_API_URL=${PRODUCT_API_URL}
+NODE_ENV=${NODE_ENV}
 EOF
+
+echo "File .env created successfully."
+
+echo "You can now run docker compose up to start the services."
+read -p "Run docker compose up? (y/n): " RUN_DOCKER_COMPOSE
+if [ "${RUN_DOCKER_COMPOSE}" == "y" ]
+then
+    docker compose up
+fi
